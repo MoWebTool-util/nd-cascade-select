cdc79166ac08d1c103539cc3b71d5948956e3725
lzhengms <lzhengms@gmail.com>
2015-06-05 18:11:13 +0800

    fix bug

--- index.js
+++ index.js
@@ -279,13 +279,15 @@ var cascadeSelect = Overlay.extend({
 
 
   _setTriggerWidth: function () {
-    var trigger = this.get('trigger');
+    var trigger = this.get('trigger'),
+      elementWidth=this.element.outerWidth(),
+      triggerOutWidth=trigger.outerWidth();
 
-    trigger.css('width', this.element.outerWidth());
+    trigger.css('width', elementWidth);
 
     // 因为 trigger 的宽度可能受 CSS（如 max-width） 限制，
     // 最后将 element 的宽度设置为与 trigger 等宽
-    this.element.css('width', trigger.outerWidth());
+    this.element.css('width', triggerOutWidth>elementWidth?triggerOutWidth:elementWidth);
   },
 
   _setHeight: function () {
